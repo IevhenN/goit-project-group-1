e13caa41b9d62853e256e9a43230a4c505acea2a
Ievhen <ievhennesterenko@gmail.com>
2024-03-17 21:19:17 +0200

    change return type getCurrencyRateAPI

--- src/main/java/banks/CurrencyTrading.java
+++ src/main/java/banks/CurrencyTrading.java
@@ -6,5 +6,5 @@ import currency.CurrencyRate;
 import java.util.List;
 
 public interface CurrencyTrading {
-    List<CurrencyRate> getCurrencyRateAPI(Currency currency);
+    CurrencyRate getCurrencyRateAPI(Currency currency);
 }
