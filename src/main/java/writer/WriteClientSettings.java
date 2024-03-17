c119826b704ced7e539bbf6e098f932ccc5d38d8
Julia-x <juliavarvaryuk@gmail.com>
2024-03-17 19:29:07 +0200

    updata

--- src/main/java/writer/WriteClientSettings.java
+++ src/main/java/writer/WriteClientSettings.java
@@ -8,13 +8,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+import static settings.Constants.SETTINGS_DIRECTORY;
+
 public class WriteClientSettings implements WriterData {
 
     private static final String FILE_EXTENSION = ".json";
 
     @Override
     public boolean writeData(ChatSettings chatSettings) {
-        String fileName = chatSettings.getChatID() + FILE_EXTENSION;
+        String fileName = SETTINGS_DIRECTORY + chatSettings.getChatID() + FILE_EXTENSION;
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try (FileWriter writer = new FileWriter(fileName)) {
             gson.toJson(chatSettings, writer);
