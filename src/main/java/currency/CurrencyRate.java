6eee258e5d8e31339c741b2f7256c83c95a432ff
Ievhen <ievhennesterenko@gmail.com>
2024-03-20 17:52:25 +0200

    example bank

--- src/main/java/currency/CurrencyRate.java
+++ src/main/java/currency/CurrencyRate.java
@@ -4,4 +4,27 @@ public class CurrencyRate {
     Currency currency;
     double buy;
     double sell;
+
+    public CurrencyRate(Currency currency, double buy, double sell) {
+        this.currency = currency;
+        this.buy = buy;
+        this.sell = sell;
+    }
+
+    public double getBuy() {
+        return buy;
+    }
+
+    public double getSell() {
+        return sell;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "=" + currency +
+                ", buy=" + buy +
+                ", sell=" + sell +
+                '}';
+    }
 }
