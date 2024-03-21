1fc0092015ff35eb694cda2384c4be1fa09f0a46
Ievhen <ievhennesterenko@gmail.com>
2024-03-21 20:51:32 +0200

    set key currency

--- src/main/java/currency/Currency.java
+++ src/main/java/currency/Currency.java
@@ -1,9 +1,17 @@
 package currency;
 
 public enum Currency {
-    USD,
-    EUR,
-    UAH;
+    USD ("840"),
+    EUR ("978"),
+    UAH ("980");
 
+    private String code;
 
+    Currency(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
--- src/main/java/settings/Constants.java
+++ src/main/java/settings/Constants.java
@@ -1,10 +1,14 @@
 package settings;
 
+import currency.Currency;
+
 public class Constants {
     static String BOT_NAME;
     static String BOT_TOKEN;
 
     public static String CHECKBOX ="✅";
     public static final String SETTINGS_DIRECTORY = "Data/";
+    public static final String CONFIG_FILE_NAME = "init.json";
+    public static final Currency NACIONAL_CURRENCY = Currency.UAH;
 
 }
